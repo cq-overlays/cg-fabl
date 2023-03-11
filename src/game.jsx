a6a255b1020f2b30029c7f6a1bef9d3f88db2ff0
LeptoFlare <47790183+LeptoFlare@users.noreply.github.com>
2023-03-11 15:58:21 -0500

    show/hide scores / commentators works + automatic commentator cycle

--- src/game.jsx
+++ src/game.jsx
@@ -4,20 +4,25 @@ import clsx from "clsx"
 import {
   useCurrentBlock,
   useCurrentColors,
+  useCurrentGameScreen,
   useCurrentRound,
   useCurrentScores,
   useCurrentTeams,
 } from "./replicants"
+import { forwardRef } from "react"
+
+const cycle = [10, 50]
 
 function App() {
+  const game = useCurrentGameScreen()
   return (
-    <AnimatePresence>
-      <div class="flex flex-col m-6 gap-6 items-stretch text-white text-2xl">
-        <Scoreboard />
-        <CurrentRound />
-        {/* <BlockSection /> */}
-      </div>
-    </AnimatePresence>
+    <div class="flex flex-col m-6 gap-6 items-stretch text-white text-2xl">
+      <AnimatePresence>{game.showScores && <Scoreboard />}</AnimatePresence>
+      <AnimatePresence>{game.showScores && <CurrentRound />}</AnimatePresence>
+      <AnimatePresence>
+        {game.showScores && game.showCommentators && <BlockSection />}
+      </AnimatePresence>
+    </div>
   )
 }
 
@@ -47,14 +52,26 @@ const Score = ({ team, score, color }) => (
 const CurrentRound = () => {
   const round = useCurrentRound()
   return (
-    <Section className="font-medium text-center -mt-3">{round.name}</Section>
+    <Section className="font-medium text-center -mt-4">{round.name}</Section>
   )
 }
 
 const BlockSection = () => {
   const block = useCurrentBlock()
   return (
-    <motion.div>
+    <motion.div
+      animate={{
+        x: [-20, 0, 0, -20],
+        opacity: [0, 1, 1, 0],
+      }}
+      transition={{
+        type: "spring",
+        duration: cycle[0],
+        times: [0, 0.2 / cycle[0], 1 - 0.2 / cycle[0], 1],
+        repeat: Infinity,
+        repeatDelay: cycle[1],
+      }}
+    >
       <Section className="flex flex-col gap-4">
         <Comm comm={block.value[0]} />
         <Comm comm={block.value[1]} />
@@ -74,14 +91,13 @@ const Comm = ({ comm }) => (
   </div>
 )
 
-const Section = ({ className, children }) => (
+const Section = forwardRef(({ className, children }, ref) => (
   <motion.div
-    transition={{
-      velocity: 0.5,
-    }}
+    ref={ref}
+    layout="position"
     initial={{ x: -20, opacity: 0 }}
     animate={{ x: 0, opacity: 1 }}
-    exit={{ x: 20, opacity: 0 }}
+    exit={{ x: -20, opacity: 0 }}
     className={clsx(
       "bg-fabl-indigo-darker rounded-xl p-4 w-full max-w-sm box-content",
       className
@@ -89,6 +105,6 @@ const Section = ({ className, children }) => (
   >
     {children}
   </motion.div>
-)
+))
 
 render(App)
